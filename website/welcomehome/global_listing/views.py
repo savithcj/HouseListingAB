6e9be55771acacd3ad83ca7b05e0cafd637b5fd7
Savith Jayasekera <scjayase@ucalgary.ca>
2019-02-21 16:11:25 -0700

    modified main app

--- website/welcomehome/global_listing/views.py
+++ website/welcomehome/global_listing/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import PropertyListing
+
+class IndexView(generic.ListView):
+    template_name = "global_listing/index.html"
+    model = PropertyListing
+
+    def get_queryset(self):
+        return PropertyListing.objects
+
+class DetailView(generic.DetailView):
+    template_name = "global_listing/detail.html"
+    model = PropertyListing
+
 
 # Create your views here.
