013adbc0b9fac99981712e93af176ae58078d97e
email <oscar.chen1@ucalgary.ca>
2019-03-20 13:12:11 -0600

    code clean up

--- website/welcomehome/welcomehome/urls.py
+++ website/welcomehome/welcomehome/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
@@ -26,7 +27,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
 ]
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
