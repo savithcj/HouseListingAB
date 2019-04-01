d57643c5aab696355be74a6619c7ffc27d1bda13
Savith Jayasekera <scjayase@ucalgary.ca>
2019-04-01 14:48:46 -0600

    added sample frontend

---
+++ website/Frontend/welcomehome/src/components/GlobalListing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GlobalListing extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Global Listing</h1>
+      </div>
+    );
+  }
+}
+
+export default GlobalListing;
