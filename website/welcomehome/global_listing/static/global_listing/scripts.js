f1586097386c75c32568b5380ff67e4176f5e2e9
Savith Jayasekera <scjayase@ucalgary.ca>
2019-04-05 19:57:53 -0600

    fixed shape and flooring info

---
+++ website/welcomehome/global_listing/static/global_listing/scripts.js
@@ -0,0 +1,54 @@
+function getFloorType(typeAsInt, id){
+
+    type = "";
+
+    switch (typeAsInt) {
+        case 0:
+            type = "Carpet";
+            break;
+        case 1:
+            type = "Laminated";
+            break;
+        case 2:
+            type = "Hard Wood";
+            break;
+        case 3:
+            type = "Wood/Polymer Composite";
+            break;
+        case 4:
+            type = "Vinyl";
+            break;
+        case 5:
+            type = "Painted/Epoxy";
+            break;
+        case 6:
+            type = "Unfinished Concrete";
+            break;
+        case 7:
+            type = "Other";
+    }
+
+    document.getElementById(id).innerHTML = type;
+}
+
+
+function getShapeType(typeAsInt, id){
+
+    type = "";
+
+    switch (typeAsInt) {
+        case 0:
+            type = "Square/Rectangle";
+            break;
+        case 1:
+            type = "Round";
+            break;
+        case 2:
+            type = "Irregular";
+            break;
+        case 3:
+            type = "Other";
+    }
+
+    document.getElementById(id).innerHTML = type;
+}
